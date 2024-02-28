aada1732fe62b70c617a3b2dae6cc239fd24be7a
Julietadallapozza <dallapozzajulieta@gmail.com>
2024-02-28 15:41:02 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,71 @@
 
 import { useState } from 'react'
 import './App.css'
+import { Square } from './components/square'
+import {TURNS, WINNER_COMBOS} from "./constants.js"
+import { checkWinnerFrom, checkEndGame } from './logic/board.js'
+import { WinnerModal } from './components/winnerModal.jsx'
+import confetti from 'canvas-confetti'
 
 
-const TURNS = {
-  X: 'x',
-  O: 'o'
-}
-
-//square component should be in another file for best practices
-const Square = ({ children, isSelected, updateBoard, index }) => {
-
-  const className = `square ${isSelected ? 'is-selected' : ''}`
-
-  const handleClick = () => {
-    updateBoard(index)
-  }
-
-  return (
-    <div onClick={handleClick} className={className}>
-      {children}
-    </div>
-  )
-}
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null)) //initialised state
   console.log(board)
 
   const [turn, setTurn] = useState(TURNS.X) //state to check who is next
+ 
+  // null es que no hay ganador, false es que hay un empate
   const [winner, setWinner] = useState(null)
 
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+
+    resetGameStorage()
+  }
+
+
+
   const updateBoard = (index) => {
-    
-    if (board[index])return
-  //actualizar board
+
+    if (board[index] || winner) return
+    //actualizar board
     const newBoard = [...board] //copy to not modify original
     newBoard[index] = turn //x or o the turn is saved here
     setBoard(newBoard)
-
     //change turn
-    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X 
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+
+    const newWinner = checkWinnerFrom(newBoard)
+    if (newWinner) {
+      confetti()
+      setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false) // empate
+    }
   }
+
+
+  
 
   return (
     <main className='board'>
-      <h1>tic tac toe</h1>
+      <h1>Tic tac toe</h1>
+      <button onClick={resetGame}>Reset del juego</button>
       <section className='game'>
         {
-          board.map((_, index) => {
+          board.map((square, index) => {
             return (
               <Square
                 key={index}
                 index={index}
                 updateBoard={updateBoard} //we are passing the function not executing it. WE DONT EXCECUTE IT BC WILL PASS 9 TIMES THE FUNCTION// WE want to excecute it when click it
               >
-                {board[index]}
+                {square}
               </Square>
             )
           })
@@ -70,11 +79,9 @@ function App() {
         <Square isSelected={turn === TURNS.O}>
           {TURNS.O}
         </Square>
-
-
-
-
       </section>
+
+      <WinnerModal resetGame={resetGame} winner={winner}/>
     </main>
   )
 
